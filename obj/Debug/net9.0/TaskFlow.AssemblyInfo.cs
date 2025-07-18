ac875e4b13d4df0308d6f1927aa0f4d51e048270
Mayowa Oladimeji <dimejikassie@gmail.com>
2025-07-18 10:02:38 +0100

    dashboard

--- obj/Debug/net9.0/TaskFlow.AssemblyInfo.cs
+++ obj/Debug/net9.0/TaskFlow.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskFlow")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2ded8d3646c3995f591ff66381cfdb2f342d720")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f764be47559d3d6a93fac5dfaca0d4e6d7e453dd")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskFlow")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskFlow")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
