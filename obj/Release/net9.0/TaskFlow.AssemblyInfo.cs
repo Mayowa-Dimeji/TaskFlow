0851b4ae438eebdf6e26422dec2956148187b08c
Mayowa Oladimeji <dimejikassie@gmail.com>
2025-07-22 12:14:41 +0100

    yml

--- obj/Release/net9.0/TaskFlow.AssemblyInfo.cs
+++ obj/Release/net9.0/TaskFlow.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskFlow")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+104eb53519953a255bd32817f36c8788ca79603f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed2c301cb12c721c0438531580731d3e66d60035")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskFlow")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskFlow")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
