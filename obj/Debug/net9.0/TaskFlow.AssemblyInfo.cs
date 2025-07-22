aec5ddf4299e1908411975bf0ee87fab5f16bf8f
Mayowa Oladimeji <dimejikassie@gmail.com>
2025-07-22 02:06:49 +0100

    mvp

--- obj/Debug/net9.0/TaskFlow.AssemblyInfo.cs
+++ obj/Debug/net9.0/TaskFlow.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskFlow")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f46787e3a0936a7aacb70a513797f34892b42719")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c21158c9ac2b5b7d7b05f5b403236ca3d921725f")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskFlow")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskFlow")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
