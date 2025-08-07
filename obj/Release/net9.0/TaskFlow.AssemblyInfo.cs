48a978a4e9d6adebbcb5507e03784cd6dfa12011
Mayowa Oladimeji <dimejikassie@gmail.com>
2025-08-07 09:43:51 +0100

    ui changes

--- obj/Release/net9.0/TaskFlow.AssemblyInfo.cs
+++ obj/Release/net9.0/TaskFlow.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskFlow")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29f548d945f29399080e4b3da32f10267a160c0d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0bb9c61cacdfe72e2d769d809c3bcf2c14011bc")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskFlow")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskFlow")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
